f9a9934b3933a02661d7de6cd013668170657d0c
John J John <johnthecoda@gmail.com>
2026-03-20 02:55:26 +0300

    Add files via upload
    New Breath and A new Lokk!

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.getElementById("open-menu");
+const sideBar = document.getElementById("sidebar");
+const menuIcon = toggleBtn.querySelector("i");
+
+toggleBtn.addEventListener("click", () => {
+  sideBar.classList.toggle("active");
+  if (sideBar.classList.contains("active")) {
+    menuIcon.classList.replace("fa-list", "fa-times");
+  } else {
+    menuIcon.classList.replace("fa-times", "fa-list");
+  }
+});
